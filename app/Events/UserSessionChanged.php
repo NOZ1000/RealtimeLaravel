2c619692c68ba2a3e5bb04ababbf7fb7f1af20a6
NOZi <madiyar1000@mail.ru>
2022-12-28 20:53:04 +0600

    16 lesson

--- app/Events/UserSessionChanged.php
+++ app/Events/UserSessionChanged.php
@@ -37,6 +37,8 @@ class UserSessionChanged implements ShouldBroadcast
      */
     public function broadcastOn()
     {
+        \Log::debug($this->message);
+        \Log::debug($this->type);
         return new PrivateChannel('notifications');
     }
 }
